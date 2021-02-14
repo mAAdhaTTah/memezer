1b472326530e5efc902c2c7f20310b9867390c2f
mAAdhaTTah <jamesorodig@gmail.com>
2021-02-14 16:34:02 -0500

    Add memes module
    Handles API calls to fetch data from the memes resource.

--- web/src/api/useApiResult.spec.ts
+++ web/src/api/useApiResult.spec.ts
@@ -1,6 +1,7 @@
 import { waitFor } from "@testing-library/react";
 import { Response } from "miragejs";
 import { object, boolean, StructError } from "superstruct";
+import { shared } from "../config";
 import { renderHook } from "../testing";
 import { useApiResult } from "./useApiResult";
 
@@ -8,14 +9,15 @@ const ResponseModel = object({
   success: boolean(),
 });
 
-const DUMMY_API = "/api/test";
+const DUMMY_ROUTE = "/dummy";
+const DUMMY_KEY = shared.API_BASE + DUMMY_ROUTE;
 
 describe("useApiResult", () => {
   it("should transition from loading -> success on successful API call", async () => {
-    server.get(DUMMY_API, () => ({
+    server.get(DUMMY_ROUTE, () => ({
       success: true,
     }));
-    const { result } = renderHook(() => useApiResult(DUMMY_API, ResponseModel));
+    const { result } = renderHook(() => useApiResult(DUMMY_KEY, ResponseModel));
 
     expect(result.current).toEqual({
       type: "loading",
@@ -32,11 +34,11 @@ describe("useApiResult", () => {
 
   it("should return AxiosError if API errors", async () => {
     server.get(
-      DUMMY_API,
+      DUMMY_ROUTE,
       () => new Response(500, {}, { message: "Server is down" })
     );
 
-    const { result } = renderHook(() => useApiResult(DUMMY_API, ResponseModel));
+    const { result } = renderHook(() => useApiResult(DUMMY_KEY, ResponseModel));
 
     await waitFor(() =>
       expect(result.current).toEqual({
@@ -49,11 +51,11 @@ describe("useApiResult", () => {
   });
 
   it("should return StructError is API returns incorrect type", async () => {
-    server.get(DUMMY_API, () => ({
+    server.get(DUMMY_ROUTE, () => ({
       success: "yes",
     }));
 
-    const { result } = renderHook(() => useApiResult(DUMMY_API, ResponseModel));
+    const { result } = renderHook(() => useApiResult(DUMMY_KEY, ResponseModel));
 
     await waitFor(() =>
       expect(result.current).toEqual({
