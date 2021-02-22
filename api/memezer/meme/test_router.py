dbbeb86ed1894ed4e81d3f2f9d579a6aca1342c3
mAAdhaTTah <jamesorodig@gmail.com>
2021-02-21 21:46:55 -0500

    Add uploaded_at timestamp to Meme
    Add this to the API response.

--- api/memezer/meme/test_router.py
+++ api/memezer/meme/test_router.py
@@ -1,6 +1,8 @@
 from typing import IO
 
+from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
 from .models import Meme
 
@@ -17,7 +19,13 @@ def test_should_return_authed_users_memes(
     response = authed_client.get("/api/memes")
 
     assert response.status_code == 200
-    assert response.json() == [{"filename": meme.filename, "title": meme.title}]
+    assert response.json() == [
+        {
+            "filename": meme.filename,
+            "title": meme.title,
+            "uploaded_at": jsonable_encoder(meme.uploaded_at),
+        }
+    ]
 
 
 def test_should_require_auth_to_create_memes(client: TestClient) -> None:
@@ -26,10 +34,17 @@ def test_should_require_auth_to_create_memes(client: TestClient) -> None:
     assert response.status_code == 401
 
 
-def test_should_create_meme(authed_client: TestClient, meme_file: IO) -> None:
+def test_should_create_meme(
+    authed_client: TestClient, meme_file: IO, db: Session
+) -> None:
     response = authed_client.post(
         "/api/memes", files={"file": ("trollface.png", meme_file, "image/png")}
     )
+    meme = db.query(Meme).one()
 
     assert response.status_code == 201
-    assert response.json() == {"filename": "trollface.png", "title": "trollface.png"}
+    assert response.json() == {
+        "filename": "trollface.png",
+        "title": "trollface.png",
+        "uploaded_at": jsonable_encoder(meme.uploaded_at),
+    }
