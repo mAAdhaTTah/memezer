f9dfef826701ad1db2eb19281cce4997364a84e5
mAAdhaTTah <jamesorodig@gmail.com>
2021-03-22 23:04:51 -0400

    Add search to home page
    Now that we have a working search endpoint, and things to search,
    we can add UI to interact with it.

--- web/src/home/LoggedInView.tsx
+++ web/src/home/LoggedInView.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, TextField } from "@material-ui/core";
 import { match } from "variant";
 import { useMemes } from "../memes";
 import { ErrorView } from "./ErrorView";
@@ -6,11 +7,25 @@ import { LoadingView } from "./LoadingView";
 import { SuccessView } from "./SuccessView";
 
 export const LoggedInView = () => {
-  const { result } = useMemes();
+  const [term, setTerm] = useState("");
+  const { result } = useMemes({ term });
 
-  return match(result, {
-    loading: () => <LoadingView />,
-    success: ({ data: memes }) => <SuccessView memes={memes} />,
-    error: ({ error }) => <ErrorView error={error} />,
-  });
+  return (
+    <>
+      <Box mb={2}>
+        <TextField
+          id="term"
+          label="Search"
+          fullWidth
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </Box>
+      {match(result, {
+        loading: () => <LoadingView />,
+        success: ({ data: memes }) => <SuccessView memes={memes} />,
+        error: ({ error }) => <ErrorView error={error} />,
+      })}
+    </>
+  );
 };
