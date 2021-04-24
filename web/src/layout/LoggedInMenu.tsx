ed34230df78a76cebc08b388fff46bfb62fcc759
James DiGioia <jamesorodig@gmail.com>
2021-04-23 21:32:45 -0400

    Update menu to link to upload page (#184)

--- web/src/layout/LoggedInMenu.tsx
+++ web/src/layout/LoggedInMenu.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { UPLOAD } from "../upload";
 import { MenuLink } from "./links";
 
 export const LoggedInMenu: React.FC<{
   onMenuClick: () => void;
 }> = React.forwardRef(({ onMenuClick }, _ref) => {
   return (
-    <MenuLink onClick={onMenuClick} to={"#"}>
-      Account Settings
+    <MenuLink onClick={onMenuClick} to={UPLOAD}>
+      Upload
     </MenuLink>
   );
 });
